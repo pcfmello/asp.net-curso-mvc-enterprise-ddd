e9080f10c45822876fca99060f2911998b29852b
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2018-08-11 15:21:52 -0300

    Pause in essentials class in 5:09:00hs

---
+++ src/PCFMELLO.CursoMvc.Application/ClienteAppService.cs
@@ -0,0 +1,58 @@
+﻿using PCFMELLO.CursoMvc.Application.Interfaces;
+using PCFMELLO.CursoMvc.Domain.Entities;
+using PCFMELLO.CursoMvc.Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCFMELLO.CursoMvc.Application
+{
+    class ClienteAppService : IClienteAppService
+    {
+        private readonly ClienteRepository _clienteRepository = new ClienteRepository();
+
+        public Cliente Adicionar(Cliente cliente)
+        {
+            return _clienteRepository.Adicionar(cliente);
+        }
+
+        public Cliente Atualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Cliente ObterPorCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cliente> ObterTodos()
+        {
+            return _clienteRepository.ObterTodos();
+        }
+
+        public void Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _clienteRepository.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/PCFMELLO.CursoMvc.Application/Interfaces/IClienteAppService.cs
@@ -0,0 +1,28 @@
+﻿using PCFMELLO.CursoMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCFMELLO.CursoMvc.Application.Interfaces
+{
+    interface IClienteAppService : IDisposable
+    {
+
+        Cliente Adicionar(Cliente cliente);
+
+        Cliente ObterPorCpf(string cpf);
+
+        Cliente ObterPorEmail(string email);
+
+        Cliente ObterPorId(Guid id);
+
+        IEnumerable<Cliente> ObterTodos();
+
+        Cliente Atualizar(Cliente cliente);
+
+        void Remover(Guid id);
+
+    }
+}
